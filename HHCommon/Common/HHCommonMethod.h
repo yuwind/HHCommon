dd93541839867e592f5d84cfd1ee70b72fcb8401
huxuewei <hxwnan@163.com>
2022-06-10 09:44:43 +0800

    add method

--- HHCommon/Common/HHCommonMethod.h
+++ HHCommon/Common/HHCommonMethod.h
@@ -9,6 +9,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef void(^HHActionBlock)(void);
+
 static inline NSString *toString(id object){
     if ([object isEqual:[NSNull null]] || object == nil || object == NULL){
         return @"";
@@ -42,6 +44,48 @@ static inline NSString *formatString(NSString *format,...){
     NSString *string = [[NSString alloc] initWithFormat:format arguments:args];
     va_end(args);
     return string;
+}
+
+static inline void performActionOnDebug(HHActionBlock actionBlock) {
+#if DEBUG || ADHOC
+    if (actionBlock) {
+        actionBlock();
+    }
+#endif
+}
+
+static inline void performActionOnRelease(HHActionBlock actionBlock) {
+#if !DEBUG && !ADHOC
+    if (actionBlock) {
+        actionBlock();
+    }
+#endif
+}
+
+static inline void performActionOnDiffEnvironment(HHActionBlock debugBlock, HHActionBlock releaseBlock) {
+#if DEBUG || ADHOC
+    if (debugBlock) {
+        debugBlock();
+    }
+#else
+    if (releaseBlock) {
+        releaseBlock();
+    }
+#endif
+}
+
+static inline void performActionOnMainThread(HHActionBlock actionBlock) {
+    if (NSThread.isMainThread) {
+        if (actionBlock) {
+            actionBlock();
+        }
+    } else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (actionBlock) {
+                actionBlock();
+            }
+        });
+    }
 }
 
 NS_ASSUME_NONNULL_END
